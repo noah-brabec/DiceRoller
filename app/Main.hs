07defd7a088c16019403a4493f1b64118a549033
Noah Brabec <noah.brabec@gmail.com>
2020-05-11 20:58:03 -0500

    Connected dsEval to dsParse

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import Lib
-import DiceEval (dsTest)
+import DiceEval (dsTest, dsEval)
 import DiceParser (parseDiceString)
 
 main :: IO ()
-main = print (parseDiceString "1+2d4+3d6")
+main = print (dsEval ([], 0) (parseDiceString "1+2d4+3d6"))
